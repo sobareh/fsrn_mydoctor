7cb810aa356bff5c7f5b9f3373c1927540f9d834
Achmad Sobari <ahmadsobarry@gmail.com>
2020-10-12 21:55:31 +0700

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import Logo from './assets/logo/logo.svg';
+import {StyleSheet, View, Text} from 'react-native';
+import Splash from './pages';
 
-export default function App() {
-  return (
-    <View style={styles.page}>
-      <Logo />
-      <Text style={styles.title}>My Doctor</Text>
-    </View>
-  );
-}
+const App = () => {
+  return <Splash />;
+};
 
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: '600',
-    color: '#112340',
-    marginTop: 20,
-  },
-});
+export default App;
+
+const styles = StyleSheet.create({});
